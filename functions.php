b0966ec0b04a6cbded05a3fc12dfcd97373af23c
ShErWooD-ForEsd <sherwooe@otc.edu>
2025-09-11 16:46:58 -0500

    bare bones of files added

---
+++ functions.php
@@ -0,0 +1,9 @@
+<!-----------------------
+Library Book Due Checker
+Name: Elizabeth Sherwood
+Due: Monday, 9/15/2025
+------------------------>
+
+<?php
+
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<!-----------------------
+Library Book Due Checker
+Name: Elizabeth Sherwood
+Due: Monday, 9/15/2025
+------------------------>
+<?php
+
+?>
