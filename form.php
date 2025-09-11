588d5959cdc27bda6e78570b9f4283494d999098
ShErWooD-ForEsd <sherwooe@otc.edu>
2025-09-11 17:00:24 -0500

    attached functions and form pages to the index page. Added bootstrap links

--- form.php
+++ form.php
@@ -7,16 +7,3 @@ Due: Monday, 9/15/2025
 <?php
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
--- index.php
+++ index.php
@@ -4,5 +4,25 @@ Name: Elizabeth Sherwood
 Due: Monday, 9/15/2025
 ------------------------>
 <?php
-
+include "functions.php";
+include "form.php";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library Book Due Checker</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+    <div>
+        <h1>Library Book Due Checker</h1>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
